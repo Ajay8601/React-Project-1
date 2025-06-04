2d3b4bbea7a72052dfbc2fa9a64fe1542e33c442
Ajay Kumar Vishwakarma <ajaykumarvishwakarma637@gmail.com>
2025-06-04 08:53:44 +0530

    Initial commit

---
+++ src/Component/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <nev className="container">
+            <div>
+                <img src="public/logo.png" alt="logo" height={50} />
+            </div>
+            <ul>
+                <a>Menu</a>
+                <a>Location</a>
+                <a>About</a>
+                <a>Contect</a>
+            </ul>
+            <button className='login-btn'>Login</button>
+        </nev>
+    </div>
+  )
+}
---
+++ src/Component/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero-container'> 
+      <div>
+        <div className='Text'>
+            <h1>
+                YOUR FEET DESERVE THE BEST
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illum vero, numquam nam, asperiores nemo natus fuga in consectetur unde optio velit similique? Voluptate.
+            </p>
+        </div>
+        <div className='logo-container'>
+            <div>
+                <button>Shop Now</button>
+            </div>
+            <div>
+                <button className='category'>Category</button>
+            </div>
+        </div>
+        <div>
+            <div className="text-2">
+                <p>
+                    Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+            <div className="logo-3">
+            <img src="public/amazon logo.jpg" alt="amazon" height={40} />
+            <img src="public/flipcart logo.jpg" alt="flipcart" height={40} />
+            </div>
+        </div>
+      </div>
+      <div className='shose'>
+        <img src="public/shose1.png" alt="shose" height={500} />
+      </div>
+    </div>
+  )
+}
